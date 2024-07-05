bfab5f7ef2b24694cf840937be85db8a61b53da7
dawko11 <dmiter90@gmail.com>
2024-07-05 19:58:04 +0200

    dodano --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,8 @@
-echo '#!/bin/bash' > skrypt.sh
-echo 'if [[ $1 == "--date" ]]; then' >> skrypt.sh
-echo '  date' >> skrypt.sh
-echo 'fi' >> skrypt.sh
+echo 'elif [[ $1 == "--logs" ]]; then' >> skrypt.sh
+echo '  num=${2:-100}' >> skrypt.sh
+echo '  for ((i=1; i<=num; i++)); do' >> skrypt.sh
+echo '    filename="log${i}.txt"' >> skrypt.sh
+echo '    echo "Filename: $filename" > $filename' >> skrypt.sh
+echo '    echo "Script: skrypt.sh" >> $filename' >> skrypt.sh
+echo '    date >> $filename' >> skrypt.sh
+echo '  done' >> skrypt.sh
