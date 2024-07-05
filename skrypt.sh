4ca8ffb123fd1cd36fac6d735872a97df897848c
dawko11 <dmiter90@gmail.com>
2024-07-05 20:11:10 +0200

    aktualizcja help

--- skrypt.sh
+++ skrypt.sh
@@ -27,3 +27,11 @@ echo '    echo "Filename: $filename" > $filename' >> skrypt.sh
 echo '    echo "Script: skrypt.sh" >> $filename' >> skrypt.sh
 echo '    date >> $filename' >> skrypt.sh
 echo '  done' >> skrypt.sh
+echo 'elif [[ $1 == "--help" || $1 == "-h" ]]; then' >> skrypt.sh
+echo '  echo "Available options:"' >> skrypt.sh
+echo '  echo "--date, -d: Display current date"' >> skrypt.sh
+echo '  echo "--logs [num], -l [num]: Create num log files (default: 100)"' >> skrypt.sh
+echo '  echo "--help, -h: Display help"' >> skrypt.sh
+echo '  echo "--init: Clone the repository and set PATH"' >> skrypt.sh
+echo '  echo "--error [num], -e [num]: Create num error files (default: 100)"' >> skrypt.sh
+
